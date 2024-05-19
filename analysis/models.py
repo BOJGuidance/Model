cc9cceb390fa2e741c5cf6fc1288d2b5e4dbdc5a
zestbear <zestbear90@gmail.com>
2024-05-20 05:09:33 +0900

    fix: option 추가

--- analysis/models.py
+++ analysis/models.py
@@ -44,6 +44,9 @@ def analysisModel(handle):
     service = Service(executable_path='/usr/local/bin/chromedriver')
     options = webdriver.ChromeOptions()
     options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument("--single-process")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(service=service, options=options)
     # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=webdriver.ChromeOptions())
 
